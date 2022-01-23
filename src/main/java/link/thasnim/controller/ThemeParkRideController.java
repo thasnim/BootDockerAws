9e42f895ebf98e1e1932acdc4f0674b17e4f9abe
thm <thasnim.hm@gmail.com>
2022-01-23 20:32:48 +0530

    initial commit

---
+++ src/main/java/link/thasnim/controller/ThemeParkRideController.java
@@ -0,0 +1,32 @@
+package link.thasnim.controller;
+
+import link.thasnim.entity.ThemeParkRide;
+import link.thasnim.repository.ThemeParkRideRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+public class ThemeParkRideController {
+    private final ThemeParkRideRepository themeParkRideRepository;
+
+    public ThemeParkRideController(ThemeParkRideRepository themeParkRideRepository) {
+        this.themeParkRideRepository = themeParkRideRepository;
+    }
+
+    @GetMapping(value = "/ride", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<ThemeParkRide> getRides(){
+        return themeParkRideRepository.findAll();
+    }
+
+    @GetMapping(value = "/ride/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ThemeParkRide getRide(@PathVariable long id){
+        return themeParkRideRepository.findById(id).orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Invalid ride id %s", id)));
+    }
+
+    @PostMapping(value = "/ride", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ThemeParkRide createRide(@RequestBody ThemeParkRide themeParkRide){
+        return themeParkRideRepository.save(themeParkRide);
+    }
+}
---
+++ src/main/java/link/thasnim/repository/ThemeParkRideRepository.java
@@ -0,0 +1,12 @@
+package link.thasnim.repository;
+
+import link.thasnim.entity.ThemeParkRide;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ThemeParkRideRepository extends CrudRepository<ThemeParkRide, Long> {
+    List<ThemeParkRide> findByName(String name);
+}
